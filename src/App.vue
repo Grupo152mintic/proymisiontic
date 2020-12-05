b3b17780277d00b9861d61d3abae72815653ffb2
jonhhollman <jonh_hollman@hotmail.com>
2020-12-05 16:53:17 -0500

    primera prueba

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    
+  <div class= "container-fluid">
+    <div class= "row justify-content- mb-5" v-for="(item, index) of equipo" :key="index">
+      <team-card v-bind: member="item" >
+
+
+      </team-card>
+  
+  </div>
+  </div>
+
+
+  </div>
+</template>
+
+<script>
+import TeamCard from './components/TeamCard.vue';
+
+export default {
+  components: { TeamCard },
+  name: "App",
+
+  data() {
+    return;
+    equipo[
+      ({
+        codigo: 1,
+        nombre: "John Hollman Gómez",
+        descripcion:
+          "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+        rol: "desarrollador",
+        imagen: "./images/jhg.jpg",
+      },
+      {
+        codigo: 2,
+        nombre: "Jose Yara",
+        descripcion:
+          "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+        rol: "desarrollador",
+        imagen: "./images/joseyara.jpg",
+      },
+      {
+        codigo: 3,
+        nombre: "John Lopez",
+        descripcion:
+          "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+        rol: "desarrollador",
+        imagen: "./images/johnlopez.jpg",
+      },
+      {
+        codigo: 4,
+        nombre: "German Ruiz",
+        descripcion:
+          "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+        rol: "desarrollador",
+        imagen: "./images/germanruiz.jpg",
+      })
+    ];
+  },
+};
+</script>
+
