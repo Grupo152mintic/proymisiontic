a5b43d2a7a125930990b1cf72648ba5b5cac93d1
germanruizh <garuizh@ut.edu.co>
2020-12-06 00:58:08 -0500

    Componente de la seccion de Equipo (Miembros del equipo)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,64 @@
 <template>
   <div id="app">
+    <div id="team">
+      <div class="card-deck">
+        <div class="card text-white bg-primary mb-4" v-for="(item, index) of team" :key="index">
+          <team-card v-bind:member="item"></team-card>
+        </div>
+      </div>
+    </div>
     <page-footer></page-footer>
-    <h1>{{ title }}</h1>
   </div>
 </template>
 
 <script>
-
-import PageFooter from './components/PageFooter.vue'
+import PageFooter from "./components/PageFooter.vue";
+import TeamCard from "./components/TeamCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    PageFooter
+    PageFooter,
+    TeamCard,
   },
-  data(){
-    return{
-      title: 'Vivo dentro de VUE'
-      }
-  }
-}
+  data() {
+    return {
+      team:[
+        {
+          codigo: 1,
+          nombre: "John Hollman Gómez",
+          descripcion:
+            "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+          rol: "Desarrollador Full-Stack",
+          imagen: "./images/johnlopez.jpg",
+        },
+        {
+          codigo: 2,
+          nombre: "José Yara",
+          descripcion:
+            "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+          rol: "Desarrollador Full-Stack",
+          imagen: "./images/joseyara.jpg",
+        },
+        {
+          codigo: 3,
+          nombre: "John López",
+          descripcion:
+            "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+          rol: "Desarrollador Full-Stack y Scrum Máster",
+          imagen: "./images/johnlopez.jpg",
+        },
+        {
+          codigo: 4,
+          nombre: "Germán Ruiz",
+          descripcion:
+            "|IT |Gerencia de Proyectos| Ingeniero de Sistemas| Director de Sistemas| Gestión de Proyectos| Planificación Estratégica | Liderazgo de Equipos | Innovación Tecnológica| Jefe de Sistemas|Manejo de Indicadores|",
+          rol: "Arquitecto de Software",
+          imagen: "./images/germanruiz.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,16 +1,22 @@
 <template>
-    <div>
-
+  <div>
+    <img v-bind:src="member.imagen" class="card-img-top" alt="Miembro" />
+    <div class="card-body">
+      <h5 class="card-title">{{ member.nombre }}</h5>
+      <p class="card-text">
+          <span>Código: </span>{{ member.codigo }}<br>
+          <p>{{ member.descripcion }}</p>
+          <p>{{ member.rol }}</p>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "TeamCard",
-        props: ['member']
-    }
+export default {
+  name: "TeamCard",
+  props: ['member'],
+};
 </script>
 
 <style scoped>
-
 </style>
